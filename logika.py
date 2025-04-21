43d7d21399ca91ed09099f460edb035cefcf9889
yayanndhr <yayannadhirkalamm@gmail.com>
2025-04-21 18:48:52 +0800

    Add files via upload

---
+++ logika.py
@@ -0,0 +1,39 @@
+x = True
+y = False
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = False
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = True
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = True
+y = False
+z = x or y
+print(x, 'or', y, '=', z)
+
+x = False
+y = True
+z = x or y
+print(x, 'or', y, '=', z)
+
+x = False
+y = False
+z = x or y
+print(x, 'or', y, '=', z)
+
+x = True
+y = True
+z = x or y
+print(x, 'or', y, '=', z)
