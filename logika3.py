047be5959de90bccf691c27e384ea9f05f22204e
yayanndhr <yayannadhirkalamm@gmail.com>
2025-06-20 12:10:36 +0800

    Add files via upload

---
+++ logika3.py
@@ -0,0 +1,7 @@
+tempat_lahir= input("masukan tempat anda lahir")
+panjang= len(tempat_lahir)
+print (panjang)
+
+len(password)>= tempat_lahir.find("e")
+print("password di terima")
+print("password kurang panjang")
